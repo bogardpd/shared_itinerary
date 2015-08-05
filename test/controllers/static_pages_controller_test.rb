a28dac5e4cc4adc1531b043c33a62a75b58c841f
Paul Bogard <paulbogard@gmail.com>
2015-08-04 22:49:34 -0400

    Add instance variable to static page test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+    @base_title = "GroupItinerary"
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home - GroupItinerary"
+    assert_select "title", "Home - #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help - GroupItinerary"
+    assert_select "title", "Help - #{@base_title}"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About - GroupItinerary"
+    assert_select "title", "About - #{@base_title}"
   end
 
 end
