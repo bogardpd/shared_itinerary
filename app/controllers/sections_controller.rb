dc18af0e469df45acf30e5d0e7c20debc76f20ef
Paul Bogard <paulbogard@gmail.com>
2015-11-26 11:20:42 -0500

    Add some form fields for new section

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,6 +2,19 @@ class SectionsController < ApplicationController
 
   def new
     @section = Event.find(params[:event]).sections.build
+    case params[:direction]
+    when "arrival"
+      @title_text = "Add a New Arrival Itinerary"
+      @is_arrival = true
+      @show_arrival_departure = false
+    when "departure"
+      @title_text = "Add a New Departure Itinerary"
+      @is_arrival = false
+      @show_arrival_departure = false
+    else
+      @title_text = "Add a New Itinerary"
+      @show_arrival_departure = true
+    end
   end
   
 end
