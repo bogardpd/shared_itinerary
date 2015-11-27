15f372308b23b0e77acf5717aeb22510ae1fc052
Paul Bogard <paulbogard@gmail.com>
2015-11-26 23:24:39 -0500

    Update New Section form to be sensitive to direction of travel

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -5,14 +5,17 @@ class SectionsController < ApplicationController
     case params[:direction]
     when "arrival"
       @title_text = "Add a New Arrival Itinerary"
+      @to_from_text = "from"
       @is_arrival = true
       @show_arrival_departure = false
     when "departure"
       @title_text = "Add a New Departure Itinerary"
+      @to_from_text = "to"
       @is_arrival = false
       @show_arrival_departure = false
     else
       @title_text = "Add a New Itinerary"
+      @to_from_text = "to/from"
       @show_arrival_departure = true
     end
   end
