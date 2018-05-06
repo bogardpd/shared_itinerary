ffc816c06680cb9e4708fb145fbec12136f55019
Paul Bogard <paulbogard@gmail.com>
2018-05-06 13:39:20 -0400

    Add airline icons to itinerary table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,26 @@
 module ApplicationHelper
 
+  def airline_icon(airline_iata, show_blank_icon: true)
+    icon_path = Airline.icon_path(airline_iata)
+    if icon_path
+      return image_tag(icon_path, class: "icon")
+    elsif show_blank_icon
+      blank_path = "#{ExternalImage::ROOT_PATH}/flights/airline-icons/unknown-airline.png"
+      return image_tag(blank_path, class: "icon")
+    else
+      return ""
+    end    
+  end
+  
+  def admin_link
+    count = Airline.where(name: nil).count + Airport.where(name: nil).count
+    if count > 0
+      link_to(%Q(Admin <span class="unread">#{count}</span>).html_safe, admin_path, class: "admin-attention")
+    else
+      link_to "Admin", admin_path
+    end
+  end
+
   # Returns the full title on a per-page basis
   def full_title(page_title = '')
     base_title = "Shared Itinerary"
@@ -27,13 +48,6 @@ module ApplicationHelper
     dt.strftime("%H:%M")
   end
   
-  def admin_link
-    count = Airline.where(name: nil).count + Airport.where(name: nil).count
-    if count > 0
-      link_to(%Q(Admin <span class="unread">#{count}</span>).html_safe, admin_path, class: "admin-attention")
-    else
-      link_to "Admin", admin_path
-    end
-  end
+  
 
 end
--- app/models/airline.rb
+++ app/models/airline.rb
@@ -9,4 +9,11 @@ class Airline < ActiveRecord::Base
     self.name ? self.name : self.iata_code
   end
   
+  # Accepts an airline IATA code and returns an image path (if the image
+  # exists) or nil (if it does not)
+  def self.icon_path(airline_iata)
+    icon_path = "#{ExternalImage::ROOT_PATH}/flights/airline-icons/#{airline_iata.upcase}.png"
+    return ExternalImage.exists?(icon_path) ? icon_path : nil
+  end
+  
 end
