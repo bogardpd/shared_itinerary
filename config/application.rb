33a37521b81a66a5431003f4649cd09563a33c51
Paul Bogard <paulbogard@gmail.com>
2018-05-27 09:44:15 -0400

    Change development environment variables location

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,13 @@ module GroupItinerary
     # config.i18n.default_locale = :de
     
     Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
+    
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
 
   end
 end
