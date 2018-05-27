c5c519f287627e8e6cdbcf1ed237fff39c02bf41
Paul Bogard <paulbogard@gmail.com>
2018-05-27 10:19:22 -0400

    Create FlightXML airport_info method

--- app/modules/flight_xml.rb
+++ app/modules/flight_xml.rb
@@ -7,6 +7,17 @@ module FlightXML
     return Savon.client(wsdl: "https://flightxml.flightaware.com/soap/FlightXML2/wsdl", basic_auth: [ENV["FLIGHTAWARE_USERNAME"], ENV["FLIGHTAWARE_API_KEY"]])
   end
   
-  
+  # Accepts an airport IATA or ICAO code, and returns a FlightXML hash of info
+  # about the airport.
+  def self.airport_info(airport_code)
+    begin
+      info = client.call(:airport_info, message: {
+        airport_code: airport_code
+        }).to_hash[:airport_info_results][:airport_info_result]
+      return info
+    rescue
+      return nil
+    end
+  end
   
 end
