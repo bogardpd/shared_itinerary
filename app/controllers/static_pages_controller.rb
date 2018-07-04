d17dc700ba416bf3a57173ab458e6bd963579cdf
Paul Bogard <paulbogard@gmail.com>
2018-07-04 13:58:01 -0400

    Write google_places_api_proxy in StaticPagesController

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,5 +12,16 @@ class StaticPagesController < ApplicationController
   def letsencrypt
     render plain: ENV["LETS_ENCRYPT_KEY"]
   end
+
+  # Interacts with Google Places API on behalf of in-page scripts to avoid
+  # sharing the API key with client-side scripts
+  def google_places_api_proxy
+    require "net/http"
+
+    url = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=#{URI.encode(params[:input])}&types=(cities)&key=#{ENV["GOOGLE_MAPS_API_KEY"]}"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    render json: response
+  end
  
 end
