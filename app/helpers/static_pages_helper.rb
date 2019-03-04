b8e1da2340d3782da0f9ba25e2d30a2c9c18820e
Paul Bogard <paulbogard@gmail.com>
2019-03-04 15:58:18 -0500

    Remove html_safe from StaticPagesHelper

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -3,7 +3,7 @@ module StaticPagesHelper
   def manage_airlines_link
     count = Airline.where(name: nil).count
     if count > 0
-      link_to(%Q(Manage airlines <span class="badge badge-light">#{count}</span>).html_safe, airlines_path, class: "btn btn-warning")
+      link_to(ActiveSupport::SafeBuffer.new + "Manage airlines " + content_tag(:span, count, class: %w(badge badge-light)), airlines_path, class: "btn btn-warning")
     else
       link_to("Manage airlines", airlines_path, class: "btn btn-outline-primary")
     end
@@ -12,7 +12,7 @@ module StaticPagesHelper
   def manage_airports_link
     count = Airport.where(needs_review: true).count
     if count > 0
-      link_to(%Q(Manage airports <span class="badge badge-light">#{count}</span>).html_safe, airports_path, class: "btn btn-warning")
+      link_to(ActiveSupport::SafeBuffer.new + "Manage airports " + content_tag(:span, count, class: %w(badge badge-light)), airports_path, class: "btn btn-warning")
     else
       link_to("Manage airports", airports_path, class: "btn btn-outline-primary")
     end
